e95026628892f9bc49edcb888693eaba3d6679da
ButchersBoy <james.willock@mulhollandsoftware.com>
2016-10-26 14:14:10 +0100

    countdown animation

---
+++ XamlAnimationWorkshop.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XamlAnimationWorkshop.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CountdownStoryboard_OnCompleted(object sender, EventArgs e)
+        {
+            ((Storyboard)((ClockGroup)sender).Timeline).Begin(this);
+        }
+    }
+}
